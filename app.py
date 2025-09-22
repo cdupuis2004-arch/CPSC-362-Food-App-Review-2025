6d0c55c703368fa89a75f5c1b78249454beeaf15
CasualCaleb <cookiecaleb@gmail.com>
2025-09-21 20:58:23 -0700

    Removed test files, added an app.py file where the main program lives and runs flask.
    Added a requirements.txt where you can install all the dependencies.
    
    Incorporated Chris's html and render it with app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# Handling of the root
+@app.route("/")
+def home():
+    #   Render Chris's HTML file
+    return render_template('chris_new_file.html')
+
+
+# If the current file being ran is THIS file
+if __name__ == "__main__":
+    app.run()
---
+++ readme.txt
@@ -0,0 +1,8 @@
+# Web App Project
+
+## Setup
+
+1. Ensure you have **Python 3.11** installed.
+2. Install project dependencies using:
+   ```bash
+   pip install -r requirements.txt
