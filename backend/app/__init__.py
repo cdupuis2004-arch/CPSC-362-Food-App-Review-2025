ea1c48dd6085e07b18403ca2e7c77912e0b6d418
CasualCaleb <cookiecaleb@gmail.com>
2025-10-03 22:08:07 -0700

    Initialized an SQLAlchemy object. Added SQLAlchemy to requirements.txt

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,8 +1,18 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 # Initializes the Flask app and sets up routing
 def create_app():
     app = Flask(__name__)
+
+    # App config
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    # Init SQLAlchemy
+    db.init_app(app)
 
     from app.routes import bp
     app.register_blueprint(bp)
