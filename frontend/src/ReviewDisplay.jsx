1750ad72b9b67629810a9626ffc8508b12a1719e
Caleb <cookiecaleb@gmail.com>
2025-11-20 16:19:49 -0800

    Updated Review Display

--- frontend/src/ReviewDisplay.jsx
+++ frontend/src/ReviewDisplay.jsx
@@ -1,36 +1,61 @@
-import { useEffect, useState } from "react";
+import {useEffect, useState} from "react";
+import reviewData from './reviews.json'
 import './ReviewDisplay.css'
 
 function ReviewDisplay() {
 
     const [reviews, setReviews] = useState([]);
 
-    useEffect(() => {
+    function toStars(num) {
+        return "★".repeat(num) + "☆".repeat(5 - num);
+    }
+
+    useEffect( () => {
+        function GetReviews() {
+            const data = reviewData
+
+            const parsed = data.map(r => ({
+                name: r.name,
+                business: r.business,
+                comment: r.comment,
+                rating: toStars(r.rating)
+            }));
+
+            setReviews(parsed)
+        }
+
+        GetReviews()
+    }, []);
+
+    // ------ Uncomment to fetch backend ------
+    /*  useEffect(() => {
         async function getReviews() {
             const response = await fetch('http://localhost:5000/api/reviews');
             const data = await response.json();
 
             const parsed = data.map(r => ({
                 name: r.name,
-                comment: r.comment
+                business: r.business,
+                comment: r.comment,
+                rating: toStars(r.rating)
             }));
 
             setReviews(parsed);
         }
 
         getReviews();
-    }, []); // empty array = run once on mount
+    }, []);*/
 
     return (
         <div className="review-container-placer">
             <h2>Reviews</h2>
-
             <div className="reviewContainer">
                 <div className="reviewScroll">
                     {reviews.map((r, idx) => (
                         <div className="review" key={idx}>
                             <h3>{r.name}</h3>
-                            <p>{r.comment}</p>
+                            <p><a>{r.business}:</a><br />{r.comment}</p>
+                            <h3>{r.rating}</h3>
                         </div>
                     ))}
                 </div>
