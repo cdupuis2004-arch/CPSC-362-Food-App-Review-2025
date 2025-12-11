bfd8686f0bdf5a6a147c02db6f5b6d93e7e17613
22tngo6 <157432110+22tngo6@users.noreply.github.com>
2025-12-11 00:20:00 -0800

    Revert "Front end test"
    This reverts commit 71c9f1702a444408a9daf8202d132943a86edd27.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,10 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import 'leaflet/dist/leaflet.css';
-import './builder-settings.js';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
     <App />
-  </React.StrictMode>,
+  </StrictMode>,
 )
