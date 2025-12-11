71c9f1702a444408a9daf8202d132943a86edd27
22tngo6 <157432110+22tngo6@users.noreply.github.com>
2025-12-11 00:14:16 -0800

    Front end test
    frontend test

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import 'leaflet/dist/leaflet.css';
+import './builder-settings.js';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>,
 )
